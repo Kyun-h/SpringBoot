ca431a6f545dfbae2e397900d54fc71d382a42dd
Kyun-h <qwertyuiopassdf48@gmail.com>
2025-04-20 20:10:22 +0900

    new

---
+++ src/test/java/haedal_SpringBoot/HaedalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package haedal_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaedalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
